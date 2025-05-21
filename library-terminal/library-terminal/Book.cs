64ca937ba007980afb3f1b5bc0f0e2a714b640ef
Zhane Moorman-Taylor <Zhaneworkmt@gmail.com>
2025-05-21 19:50:29 -0400

    push

--- library-terminal/library-terminal/Book.cs
+++ library-terminal/library-terminal/Book.cs
@@ -1,7 +1,20 @@
-namespace library_terminal;
+namespace ConsoleApp1;
 
-public class Book
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Book
 {
-   
-}
-   
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Status { get; set; } = "On Shelf";
+    public DateTime? DueDate { get; set; } = null;
+
+    public override string ToString()
+    {
+        string due = DueDate.HasValue ? DueDate.Value.ToShortDateString() : "N/A";
+        return $"{Title,-30} | {Author,-20} | {Status,-12} | Due: {due}";
+    }
+}  
