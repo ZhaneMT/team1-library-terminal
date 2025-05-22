cd7a8c22a4175c6641db8319e7725786e6c84b23
sean carter <ajacks75@emich.edu>
2025-05-21 20:17:51 -0400

    backlog

--- library-terminal/library-terminal/Book.cs
+++ library-terminal/library-terminal/Book.cs
@@ -5,16 +5,27 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+
+
+
+/*
+ *  Your solution must include some kind of a book class with a title, author, status, and due
+   date if checked out.
+   o Status should be On Shelf or Checked Out (or other statuses you can imagine).
+   
+ */
 class Book
 {
     public string Title { get; set; }
     public string Author { get; set; }
-    public string Status { get; set; } = "On Shelf";
+    public bool onShelfStatus  = true;
+    public bool checkedoutStatus  = false;
+    
     public DateTime? DueDate { get; set; } = null;
 
     public override string ToString()
     {
         string due = DueDate.HasValue ? DueDate.Value.ToShortDateString() : "N/A";
-        return $"{Title,-30} | {Author,-20} | {Status,-12} | Due: {due}";
+        return $"{Title,-30} | {Author,-20} | {onShelfStatus,-12} | Due: {due}";
     }
 }  
