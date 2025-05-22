79889aca590e569a14ce78ace0f2463b5dfa13be
Zhane Moorman-Taylor <Zhaneworkmt@gmail.com>
2025-05-21 20:19:28 -0400

    Constructor

--- library-terminal/library-terminal/Book.cs
+++ library-terminal/library-terminal/Book.cs
@@ -1,15 +1,13 @@
-namespace ConsoleApp1;
+namespace library_terminal;
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+
 
 class Book
 {
     public string Title { get; set; }
     public string Author { get; set; }
-    public string Status { get; set; } = "On Shelf";
+    public bool Status { get; set; } 
     public DateTime? DueDate { get; set; } = null;
 
     public override string ToString()
@@ -17,4 +15,36 @@ class Book
         string due = DueDate.HasValue ? DueDate.Value.ToShortDateString() : "N/A";
         return $"{Title,-30} | {Author,-20} | {Status,-12} | Due: {due}";
     }
-}  
+    
+    //-----------------------------------------------------------------
+    // CREATION OF THE BOOK OBJECT ITSELF
+    public Book(string title, string author, bool status, string dueDate)
+    {
+        Title = title;
+        Author = author;
+        Status = status;
+        DueDate = DueDate;
+    }
+    public List<Book> Books;
+
+    public Book()
+    {
+        Books = new List<Book>();
+        {
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");
+            new Book("Title", "Author", false, "Due date");   
+        }
+    }
+    
+    //------------------------------------------------------------------
+}
--- library-terminal/library-terminal/Program.cs
+++ library-terminal/library-terminal/Program.cs
@@ -5,7 +5,22 @@ class Program
     static void Main(string[] args)
     {
       
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Welcome to the Library Terminal!");
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("1. Display all books.");
+        Console.WriteLine("2. Search by author.");
+        Console.WriteLine("3. Search by title keyword.");
+        Console.WriteLine("4. Check out a book.");
+        Console.WriteLine("5. Return a book.");
+        Console.WriteLine("6. Quit.");
+        string userInput = Console.ReadLine();
+
+        if (userInput == "1")
+        {
+            
+        }
+        
+
         
         
         
