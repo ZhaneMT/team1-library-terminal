e9ecf442981ee917d1940a92b4afcf871415517c
Zhane Moorman-Taylor <Zhaneworkmt@gmail.com>
2025-05-21 21:26:45 -0400

    okay

--- library-terminal/library-terminal/Book.cs
+++ library-terminal/library-terminal/Book.cs
@@ -1,50 +1,31 @@
-namespace library_terminal;
+namespace ConsoleApp1;
 
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 
+
+
+/*
+ *  Your solution must include some kind of a book class with a title, author, status, and due
+   date if checked out.
+   o Status should be On Shelf or Checked Out (or other statuses you can imagine).
+
+ */
 class Book
 {
     public string Title { get; set; }
     public string Author { get; set; }
-    public bool Status { get; set; } 
+    public bool onShelfStatus  = true;
+    public bool checkedoutStatus  = false;
+    
     public DateTime? DueDate { get; set; } = null;
 
     public override string ToString()
     {
         string due = DueDate.HasValue ? DueDate.Value.ToShortDateString() : "N/A";
-        return $"{Title,-30} | {Author,-20} | {Status,-12} | Due: {due}";
+        return $"{Title,-30} | {Author,-20} | {onShelfStatus,-12} | Due: {due}";
     }
-    
-    //-----------------------------------------------------------------
-    // CREATION OF THE BOOK OBJECT ITSELF
-    public Book(string title, string author, bool status, string dueDate)
-    {
-        Title = title;
-        Author = author;
-        Status = status;
-        DueDate = DueDate;
-    }
-    public List<Book> Books;
-
-    public Book()
-    {
-        Books = new List<Book>();
-        {
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");
-            new Book("Title", "Author", false, "Due date");   
-        }
-    }
-    
-    //------------------------------------------------------------------
 }
--- library-terminal/library-terminal/Program.cs
+++ library-terminal/library-terminal/Program.cs
@@ -2,25 +2,26 @@
 
 class Program
 {
+    
+/*
+ * 1. create book object properties
+ * 2. init book object properties
+ * 3.create book constructor
+ * 4.Create Library List
+ * 5. Add 12 books to the list
+ * 6.create method to display all books
+ * 7.create method to Search for a book by author
+ *8.create method to Search for a book by title keyword
+ * 9.create method to select book
+ * 10.if book is checked out let them know
+ * 11. If not, check it out to them and set the due date to 2 weeks from today.
+   12. Return a book. (You can decide how that looks/what questions it asks.)
+ *
+ */
     static void Main(string[] args)
     {
       
-        Console.WriteLine("Welcome to the Library Terminal!");
-        Console.WriteLine("What would you like to do?");
-        Console.WriteLine("1. Display all books.");
-        Console.WriteLine("2. Search by author.");
-        Console.WriteLine("3. Search by title keyword.");
-        Console.WriteLine("4. Check out a book.");
-        Console.WriteLine("5. Return a book.");
-        Console.WriteLine("6. Quit.");
-        string userInput = Console.ReadLine();
-
-        if (userInput == "1")
-        {
-            
-        }
-        
-
+        Console.WriteLine("Hello World!");
         
         
         
